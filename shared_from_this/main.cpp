b19caf7885e5d9b805840b0d965fb08d596f2543
Corentin Le Molgat <clemolgat@aldebaran-robotics.com>
2015-12-29 16:31:18 +0100

    Shared_from_this: add Interface Class

--- shared_from_this/main.cpp
+++ shared_from_this/main.cpp
@@ -11,18 +11,23 @@ struct Bad {
   ~Bad() { std::cout << "Bad::~Bad() called\n"; }
 };
 
-struct Foo : std::enable_shared_from_this<Foo> {
-  virtual ~Foo() { std::cout << "Foo::~Foo() called\n"; }
-  virtual std::shared_ptr<Foo> getPtr() {
-    std::cout << "Foo::getPtr() called\n";
+struct IFoo {
+  virtual ~IFoo() { std::cout << "IFoo::~IFoo() called\n"; }
+  virtual std::shared_ptr<IFoo> getPtr() = 0;
+};
+
+struct FooBase : std::enable_shared_from_this<FooBase>, IFoo {
+  virtual ~FooBase() { std::cout << "FooBase::~FooBase() called\n"; }
+  virtual std::shared_ptr<IFoo> getPtr() {
+    std::cout << "FooBase::getPtr() called\n";
     return shared_from_this();
   }
 };
 
-struct Bar : public Foo {
-  virtual ~Bar() { std::cout << "Bar::~Bar() called\n"; }
-  virtual std::shared_ptr<Foo> getPtr() {
-    std::cout << "Bar::getPtr() called\n";
+struct FooDerived : public FooBase {
+  virtual ~FooDerived() { std::cout << "FooDerived::~FooDerived() called\n"; }
+  virtual std::shared_ptr<IFoo> getPtr() {
+    std::cout << "FooDerived::getPtr() called\n";
     return shared_from_this();
   }
 };
@@ -38,11 +43,11 @@ int main() {
     std::shared_ptr<Bad> bp1(new Bad);
     std::shared_ptr<Bad> bp2 = bp1->getptr();
     std::cout << "bp2.use_count() = " << bp2.use_count() << '\n';
-  }
+  } // Ultra Bad: double-delete of Bad
 
   {
-    std::shared_ptr<Foo> fooPtr = std::make_shared<Bar>();
-    std::shared_ptr<Foo> foo2Ptr = fooPtr->getPtr();
+    std::shared_ptr<IFoo> fooPtr = std::make_shared<FooDerived>();
+    std::shared_ptr<IFoo> foo2Ptr = fooPtr->getPtr();
     std::cout << "foo2Ptr.use_count() = " << foo2Ptr.use_count() << '\n';
   }
-}  // UB: double-delete of Bad
+}
