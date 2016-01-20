9379e7b4311e2e2e7bb101c55844c7ad5782c3eb
mizux <dev@mizux.net>
2016-01-20 15:39:42 +0100

    add xmacro snippets

---
+++ xmacro/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <map>
+
+#define FOOBAR_LIST \
+	X(FOO), \
+	X(BAR), \
+	X(INVALID)
+
+enum class FooBar {
+#define X(name) name //!< @brief XMacro stuff.
+	FOOBAR_LIST
+#undef X
+};
+
+std::ostream& operator<<(std::ostream& stream, const FooBar& p);
+
+int main() {
+	FooBar foo = FooBar::FOO;
+	std::cout << "foo: " << foo << std::endl;
+	FooBar bar = FooBar::BAR;
+	std::cout << "bar: " << bar << std::endl;
+	FooBar invalid = FooBar::INVALID;
+	std::cout << "unknow: " << invalid << std::endl;
+
+	switch (bar) {
+		case FooBar::FOO:
+		case FooBar::INVALID:
+		default:
+			std::cout << "Here" << std::endl;
+			break;
+	} // warning since BAR is not managed with -Wswitch-enum
+}
+
+std::ostream& operator<<(std::ostream& stream, const FooBar& value) {
+	static const std::map<FooBar, std::string> fooBarNames = {
+	#define X(name) {FooBar::name, #name} //!< @brief XMacro stuff.
+		FOOBAR_LIST
+	#undef X
+	};
+
+	stream << fooBarNames.find(value)->second;
+	return stream;
+}
